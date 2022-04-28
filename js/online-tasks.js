f4140901ec03a0845cd64b6e2ab0370235dc4d7d
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-04-28 12:31:33 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -431,7 +431,7 @@ const scores27 = [89, 64, 42, 17, 93, 51, 26];
 const bestScore27 = Math.max(...scores27);
 const worstScore27 = Math.min(...scores27);
 
-3.28;
+// 3.28
 const firstGroupScores = [64, 42, 93];
 const secondGroupScores = [89, 14, 51, 26];
 const thirdGroupScores = [29, 47, 18, 97, 81];
