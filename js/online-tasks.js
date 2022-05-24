5f1f3ee639584ac56bea6d61e136d2169e300967
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-05-24 18:18:34 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -488,6 +488,7 @@ function add(...args) {
     return totalSum;
 }
 console.log(add(15, 27));
+// 42
 console.log(add(12, 4, 11, 48));
 console.log(add(32, 6, 13, 19, 8));
 console.log(add(74, 11, 62, 46, 12, 36));
