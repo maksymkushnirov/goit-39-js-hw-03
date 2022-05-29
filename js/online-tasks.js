1b7c5aa681fef8b780664412592df3e45b73764c
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-05-29 17:39:31 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -511,6 +511,7 @@ function addOverNum(number, ...args) {
 console.log(addOverNum(50, 12, 27));
 // 0
 console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+// 71
 console.log(addOverNum(15, 32, 6, 13, 19, 8));
 console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
 
