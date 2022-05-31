efedc9d4cfc0943ac48fc7d5ac122536767333a9
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-05-31 18:51:38 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -515,6 +515,7 @@ console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
 console.log(addOverNum(15, 32, 6, 13, 19, 8));
 // 51
 console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
+// 218
 
 // 3.33
 function findMatches(numbers, ...args) {
