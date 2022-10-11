97e22755ab0ee38eba44ebe79616ddb7e256b11e
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-10-11 18:18:27 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -351,6 +351,39 @@ console.log('');
 console.log('~~~ Task 3.7 ~~~');
 console.log('');
 
+/* Добавление свойств
+Операция добавления нового свойства после создания объекта ничем не отличается от 
+изменения значения уже существующего свойства. Если при записи значения по имени, 
+такого свойства в объекте нет, оно будет создано.
+const book = {
+  title: "Последнее королевство",
+  author: "Бернард Корнуэлл",
+  genres: ["историческая проза", "приключения"],
+  public: true,
+  rating: 8.38
+};
+book.pageCount = 836;
+book.originalLanguage = "en";
+book.translations = ["ua", "ru"];
+console.log(book.pageCount); // 836
+console.log(book.originalLanguage); // "en"
+console.log(book.translations); // ["ua", "ru"]
+Задание
+Добавь объекту apartment несколько новых свойств:
+area - площадь в квадратных метрах, число 60;
+rooms - количество комнат, число 3;
+location - местоположение квартиры, обьект со следующими вложенными свойствами;
+country - страна, строка "Jamaica";
+city - город, строка "Kingston".
+Тесты
+Объявлена переменная apartment
+Значение переменной apartment это объект
+Значение вложенного свойства area это число 60
+Значение вложенного свойства rooms это число 3
+Значение вложенного свойства location это объект
+Значение вложенного свойства location.country это строка "Jamaica"
+Значение вложенного свойства location.city это строка "Kingston" */
+
 const apartment7 = {
     imgUrl: 'https://via.placeholder.com/640x480',
     descr: 'Spacious apartment in the city center',
