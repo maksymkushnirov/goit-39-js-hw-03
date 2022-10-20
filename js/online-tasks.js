2cde0b65ed13b13f34e20b42990e6a9630dadea1
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-10-20 08:41:52 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -792,6 +792,19 @@ console.log('');
 console.log('~~~ Task 3.16 ~~~');
 console.log('');
 
+/* Задача. Расходы на зарплату
+Задание
+Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат, 
+где имя свойства это имя сотрудника, а значение свойства это зарплата. Функция 
+должна рассчитать общую сумму зарплат сотрудников и вернуть её. Используй переменную
+totalSalary для хранения общей суммы зарплаты.
+Тесты
+Объявлена функция countTotalSalary(salaries)
+Вызов countTotalSalary({}) возвращает 0
+Вызов countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) возвращает 330
+Вызов countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) возвращает 400
+Функция учитывает только собственные свойства объекта */
+
 function countTotalSalary16(salaries16) {
     let totalSalary16 = 0;
     const values = Object.values(salaries16);
