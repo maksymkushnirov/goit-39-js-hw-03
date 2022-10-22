147848b92f1db6ce6beae2a4336583d181a61fa5
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-10-22 19:58:26 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -889,6 +889,20 @@ console.log('');
 console.log('~~~ Task 3.18 ~~~');
 console.log('');
 
+/* Задача. Поиск объекта по значению свойства
+Задание
+Напиши функцию getProductPrice(productName) которая принимает один параметр 
+productName - название продукта. Функция ищет объект продукта с таким именем (свойство name) 
+в массиве products и возвращает его цену (свойство price). Если продукт с таким названием 
+не найден, функция должна возвращать null.
+Тесты
+Объявлена функция getProductPrice(productName).
+Вызов getProductPrice("Radar") возвращает 1300.
+Вызов getProductPrice("Grip") возвращает 1200.
+Вызов getProductPrice("Scanner") возвращает 2700.
+Вызов getProductPrice("Droid") возвращает 400.
+Вызов getProductPrice("Engine") возвращает null */
+
 const products18 = [
     { name: 'Radar', price: 1300, quantity: 4 },
     { name: 'Scanner', price: 2700, quantity: 3 },
