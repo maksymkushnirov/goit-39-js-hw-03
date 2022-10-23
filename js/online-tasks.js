cd925b736f24d0dc355f86062cfa9a84cfd22dfb
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-10-23 22:39:20 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -933,6 +933,19 @@ console.log('');
 console.log('~~~ Task 3.19 ~~~');
 console.log('');
 
+/* Задача. Коллекция значений свойства
+Задание
+Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - 
+имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем 
+из каждого объекта в массиве products. Если в объектах нет свойства с таким именем, функция 
+должна вернуть пустой массив.
+Тесты
+Объявлена функция getAllPropValues(propName)
+Вызов getAllPropValues("name") возвращает ["Radar", "Scanner", "Droid", "Grip"]
+Вызов getAllPropValues("quantity") возвращает [4, 3, 7, 9]
+Вызов getAllPropValues("price") возвращает [1300, 2700, 400, 1200]
+Вызов getAllPropValues("category") возвращает [] */
+
 const products19 = [
     { name: 'Radar', price: 1300, quantity: 4 },
     { name: 'Scanner', price: 2700, quantity: 3 },
