bc6111e2353523a8cbc8f083a1501d0b70550f00
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-10-24 14:50:19 +0300

    Update online-tasks.js

--- js/online-tasks.js
+++ js/online-tasks.js
@@ -975,6 +975,17 @@ console.log('');
 console.log('~~~ Task 3.20 ~~~');
 console.log('');
 
+/* Задача. Общая стоимость товара
+Задание
+Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+Тесты
+Объявлена функция calculateTotalPrice(productName)
+Вызов calculateTotalPrice("Blaster") возвращает 0
+Вызов calculateTotalPrice("Radar") возвращает 5200
+Вызов calculateTotalPrice("Droid") возвращает 2800
+Вызов calculateTotalPrice("Grip") возвращает 10800
+Вызов calculateTotalPrice("Scanner") возвращает 8100 */
+
 const products = [
     { name: 'Radar', price: 1300, quantity: 4 },
     { name: 'Scanner', price: 2700, quantity: 3 },
